c61325c0222653a66f276966eb5278f9910a18ca
adega <eadegafernandez@gmail.com>
2025-10-18 00:14:07 +0200

    flake module name

--- flake.nix
+++ flake.nix
@@ -3,14 +3,10 @@
 
   inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable"; };
 
-  /* outputs = inputs:
-     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-       systems = import inputs.systems;
-       imports = [ ./overleaf.nix ];
-     };
-  */
-
   outputs = inputs@{ self, nixpkgs, ... }: {
-    nixosModules.default = import ./overleaf.nix inputs;
+    nixosModules = rec {
+      default = overleaf;
+      overleaf = import ./overleaf.nix;
+    };
   };
 }
